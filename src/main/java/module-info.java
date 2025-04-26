ae9efafb912359baa0b0a05da24c6556f08f54bf
thanhduydevd <dothanhduy333@gmail.com>
2025-04-26 20:57:14 +0700

    beta1_Khoa

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,15 @@
-module edu.iuh.fit.coffee.quanlycuahangcaphe {
+module com.damcafe.app {
     requires javafx.fxml;
-
     requires com.dlsc.formsfx;
     requires animatefx;
+    requires javafx.graphics;
 
-    opens edu.iuh.fit.coffee.quanlycuahangcaphe.app to javafx.fxml;
-    exports edu.iuh.fit.coffee.quanlycuahangcaphe.app;
+    exports com.damcafe.app.controller;
+    opens com.damcafe.app.controller to javafx.fxml;
 
-    exports edu.iuh.fit.coffee.quanlycuahangcaphe.app.controller;
-    opens edu.iuh.fit.coffee.quanlycuahangcaphe.app.controller to javafx.fxml;
-    exports edu.iuh.fit.coffee.quanlycuahangcaphe.app.controller.dialog;
-    opens edu.iuh.fit.coffee.quanlycuahangcaphe.app.controller.dialog to javafx.fxml;
+    exports com.damcafe.app.controller.dialog;
+    opens com.damcafe.app.controller.dialog to javafx.fxml;
+
+    exports com.damcafe.app.gui;
+    opens com.damcafe.app.gui to javafx.fxml;
 }
